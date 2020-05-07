841fcd7b4734c2286efbd17504735eb078aeab56
ry_Tom <31386783+02ryo15@users.noreply.github.com>
2020-05-07 23:41:37 +0900

    construct the base of the mod

---
+++ src/main/java/ryoryo/cct/util/References.java
@@ -0,0 +1,21 @@
+package ryoryo.cct.util;
+
+public class References {
+	public static final String MOD_ID = "cct";
+	public static final String MOD_NAME = "CompressedCobblestoneTools";
+
+	public static final String MOD_VERSION_MAJOR = "GRADLE.VERSION_MAJOR";
+	public static final String MOD_VERSION_MINOR = "GRADLE.VERSION_MINOR";
+	public static final String MOD_VERSION_PATCH = "GRADLE.VERSION_PATCH";
+	public static final String MOD_VERSION = MOD_VERSION_MAJOR + "." + MOD_VERSION_MINOR + "." + MOD_VERSION_PATCH;
+
+	public static final String MOD_DEPENDENCIES = "required-after:forge@[14.23.5.2768,);"
+												// + "required-after:polishedlib@[1.0.2,);";
+												+ "required-after:polishedlib;";
+
+	public static final String MOD_ACCEPTED_MC_VERSIONS = "[1.12.2]";
+	public static final String MOD_GUI_FACTORY = "ryoryo.cct.config.GuiFactoryModConfig";
+
+	public static final String PROXY_CLIENT = "ryoryo.cct.proxy.ClientProxy";
+	public static final String PROXY_COMMON = "ryoryo.cct.proxy.CommonProxy";
+}
