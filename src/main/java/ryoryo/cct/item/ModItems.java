bcbb0151fff3b63daf84728c7ae48ccf6d042e63
ry_Tom <31386783+02ryo15@users.noreply.github.com>
2020-12-29 18:26:10 +0900

    add arrays of tools and change internal names of tools

--- src/main/java/ryoryo/cct/item/ModItems.java
+++ src/main/java/ryoryo/cct/item/ModItems.java
@@ -1,10 +1,15 @@
 package ryoryo.cct.item;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
 import ryoryo.cct.CompressedCobblestoneTools;
 import ryoryo.cct.util.EnumCompressed;
+import ryoryo.polishedlib.item.ItemBasePaxel;
 
 public class ModItems {
+
+	public static final Item ITEM_STONE_PAXEL = new ItemBasePaxel(ToolMaterial.STONE).setUnlocalizedName("stone_paxel").setCreativeTab(CreativeTabs.TOOLS);
 
 	public static final Item ITEM_SHOVEL_X1 = new ItemModShovel(EnumCompressed.X1);
 	public static final Item ITEM_SHOVEL_X2 = new ItemModShovel(EnumCompressed.X2);
@@ -14,6 +19,7 @@ public class ModItems {
 	public static final Item ITEM_SHOVEL_X6 = new ItemModShovel(EnumCompressed.X6);
 	public static final Item ITEM_SHOVEL_X7 = new ItemModShovel(EnumCompressed.X7);
 	public static final Item ITEM_SHOVEL_X8 = new ItemModShovel(EnumCompressed.X8);
+	public static final Item[] SHOVELS = { ITEM_SHOVEL_X1, ITEM_SHOVEL_X2, ITEM_SHOVEL_X3, ITEM_SHOVEL_X4, ITEM_SHOVEL_X5, ITEM_SHOVEL_X6, ITEM_SHOVEL_X7, ITEM_SHOVEL_X8 };
 
 	public static final Item ITEM_PICKAXE_X1 = new ItemModPickaxe(EnumCompressed.X1);
 	public static final Item ITEM_PICKAXE_X2 = new ItemModPickaxe(EnumCompressed.X2);
@@ -23,6 +29,7 @@ public class ModItems {
 	public static final Item ITEM_PICKAXE_X6 = new ItemModPickaxe(EnumCompressed.X6);
 	public static final Item ITEM_PICKAXE_X7 = new ItemModPickaxe(EnumCompressed.X7);
 	public static final Item ITEM_PICKAXE_X8 = new ItemModPickaxe(EnumCompressed.X8);
+	public static final Item[] PICKAXES = { ITEM_PICKAXE_X1, ITEM_PICKAXE_X2, ITEM_PICKAXE_X3, ITEM_PICKAXE_X4, ITEM_PICKAXE_X5, ITEM_PICKAXE_X6, ITEM_PICKAXE_X7, ITEM_PICKAXE_X8 };
 
 	public static final Item ITEM_AXE_X1 = new ItemModAxe(EnumCompressed.X1);
 	public static final Item ITEM_AXE_X2 = new ItemModAxe(EnumCompressed.X2);
@@ -32,6 +39,7 @@ public class ModItems {
 	public static final Item ITEM_AXE_X6 = new ItemModAxe(EnumCompressed.X6);
 	public static final Item ITEM_AXE_X7 = new ItemModAxe(EnumCompressed.X7);
 	public static final Item ITEM_AXE_X8 = new ItemModAxe(EnumCompressed.X8);
+	public static final Item[] AXES = { ITEM_AXE_X1, ITEM_AXE_X2, ITEM_AXE_X3, ITEM_AXE_X4, ITEM_AXE_X5, ITEM_AXE_X6, ITEM_AXE_X7, ITEM_AXE_X8 };
 
 	public static final Item ITEM_SWORD_X1 = new ItemModSword(EnumCompressed.X1);
 	public static final Item ITEM_SWORD_X2 = new ItemModSword(EnumCompressed.X2);
@@ -41,6 +49,7 @@ public class ModItems {
 	public static final Item ITEM_SWORD_X6 = new ItemModSword(EnumCompressed.X6);
 	public static final Item ITEM_SWORD_X7 = new ItemModSword(EnumCompressed.X7);
 	public static final Item ITEM_SWORD_X8 = new ItemModSword(EnumCompressed.X8);
+	public static final Item[] SWORDS = { ITEM_SWORD_X1, ITEM_SWORD_X2, ITEM_SWORD_X3, ITEM_SWORD_X4, ITEM_SWORD_X5, ITEM_SWORD_X6, ITEM_SWORD_X7, ITEM_SWORD_X8 };
 
 	public static final Item ITEM_PAXEL_X1 = new ItemModPaxel(EnumCompressed.X1);
 	public static final Item ITEM_PAXEL_X2 = new ItemModPaxel(EnumCompressed.X2);
@@ -50,51 +59,17 @@ public class ModItems {
 	public static final Item ITEM_PAXEL_X6 = new ItemModPaxel(EnumCompressed.X6);
 	public static final Item ITEM_PAXEL_X7 = new ItemModPaxel(EnumCompressed.X7);
 	public static final Item ITEM_PAXEL_X8 = new ItemModPaxel(EnumCompressed.X8);
+	public static final Item[] PAXELS = { ITEM_PAXEL_X1, ITEM_PAXEL_X2, ITEM_PAXEL_X3, ITEM_PAXEL_X4, ITEM_PAXEL_X5, ITEM_PAXEL_X6, ITEM_PAXEL_X7, ITEM_PAXEL_X8 };
 
 	public static void init() {
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_SHOVEL_X1, "shovel_compressed_x1");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_SHOVEL_X2, "shovel_compressed_x2");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_SHOVEL_X3, "shovel_compressed_x3");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_SHOVEL_X4, "shovel_compressed_x4");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_SHOVEL_X5, "shovel_compressed_x5");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_SHOVEL_X6, "shovel_compressed_x6");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_SHOVEL_X7, "shovel_compressed_x7");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_SHOVEL_X8, "shovel_compressed_x8");
+		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_STONE_PAXEL, "stone_paxel");
 
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_PICKAXE_X1, "pickaxe_compressed_x1");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_PICKAXE_X2, "pickaxe_compressed_x2");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_PICKAXE_X3, "pickaxe_compressed_x3");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_PICKAXE_X4, "pickaxe_compressed_x4");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_PICKAXE_X5, "pickaxe_compressed_x5");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_PICKAXE_X6, "pickaxe_compressed_x6");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_PICKAXE_X7, "pickaxe_compressed_x7");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_PICKAXE_X8, "pickaxe_compressed_x8");
-
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_AXE_X1, "axe_compressed_x1");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_AXE_X2, "axe_compressed_x2");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_AXE_X3, "axe_compressed_x3");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_AXE_X4, "axe_compressed_x4");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_AXE_X5, "axe_compressed_x5");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_AXE_X6, "axe_compressed_x6");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_AXE_X7, "axe_compressed_x7");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_AXE_X8, "axe_compressed_x8");
-
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_SWORD_X1, "sword_compressed_x1");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_SWORD_X2, "sword_compressed_x2");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_SWORD_X3, "sword_compressed_x3");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_SWORD_X4, "sword_compressed_x4");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_SWORD_X5, "sword_compressed_x5");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_SWORD_X6, "sword_compressed_x6");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_SWORD_X7, "sword_compressed_x7");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_SWORD_X8, "sword_compressed_x8");
-
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_PAXEL_X1, "paxel_compressed_x1");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_PAXEL_X2, "paxel_compressed_x2");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_PAXEL_X3, "paxel_compressed_x3");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_PAXEL_X4, "paxel_compressed_x4");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_PAXEL_X5, "paxel_compressed_x5");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_PAXEL_X6, "paxel_compressed_x6");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_PAXEL_X7, "paxel_compressed_x7");
-		CompressedCobblestoneTools.REGISTER.registerItem(ITEM_PAXEL_X8, "paxel_compressed_x8");
+		for(int i = 0; i < EnumCompressed.getLength(); i ++) {
+			CompressedCobblestoneTools.REGISTER.registerItem(SHOVELS[i], "compressed_stone_shovel_x" + (i + 1));
+			CompressedCobblestoneTools.REGISTER.registerItem(PICKAXES[i], "compressed_stone_pickaxe_x" + (i + 1));
+			CompressedCobblestoneTools.REGISTER.registerItem(AXES[i], "compressed_stone_axe_x" + (i + 1));
+			CompressedCobblestoneTools.REGISTER.registerItem(SWORDS[i], "compressed_stone_sword_x" + (i + 1));
+			CompressedCobblestoneTools.REGISTER.registerItem(PAXELS[i], "compressed_stone_paxel_x" + (i + 1));
+		}
 	}
 }
