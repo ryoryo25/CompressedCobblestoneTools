731b45602dc09cd83a653cc1434451f444582de0
ry_Tom <31386783+02ryo15@users.noreply.github.com>
2021-04-04 03:31:57 +0900

    use Utils#measureTime to measure proccess time

--- src/main/java/ryoryo/cct/client/handler/TextureHandler.java
+++ src/main/java/ryoryo/cct/client/handler/TextureHandler.java
@@ -1,7 +1,5 @@
 package ryoryo.cct.client.handler;
 
-import com.google.common.base.Stopwatch;
-
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.TextureStitchEvent;
@@ -14,6 +12,7 @@ import ryoryo.cct.client.texture.SpriteCompressed;
 import ryoryo.cct.client.texture.SpriteCompressedTools;
 import ryoryo.cct.util.EnumCompressed;
 import ryoryo.cct.util.References;
+import ryoryo.polishedlib.util.Utils;
 
 public class TextureHandler {
 
@@ -24,29 +23,29 @@ public class TextureHandler {
 		if (!map.getBasePath().equals("textures"))
 			return;
 
-		CompressedCobblestoneTools.LOGGER.info("Start stitching textures");
-		Stopwatch stopwatch = Stopwatch.createStarted();
-		ProgressBar stitchBar = ProgressManager.push("CCT: stitching textures", 8 + 1); // tier 1 - 8 and paxel
+		Utils.measureTime("stitching textures", CompressedCobblestoneTools.LOGGER, () -> {
 
-		float max_n = (float) Math.sqrt(EnumCompressed.getLength() * 8) + 0.5F;
+			ProgressBar stitchBar = ProgressManager.push("CCT: stitching textures", 8 + 1); // tier 1 - 8 and paxel
 
-		for (int i = 0; i < EnumCompressed.getLength(); i ++) {
-			stitchBar.step("Compressed tier " + (i + 1));
+			float max_n = (float) Math.sqrt(EnumCompressed.getLength() * 8) + 0.5F;
 
-			map.setTextureEntry(new SpriteCompressed("cobblestone", i, max_n));
-			map.setTextureEntry(new SpriteCompressedTools("stone_shovel", i, max_n));
-			map.setTextureEntry(new SpriteCompressedTools("stone_pickaxe", i, max_n));
-			map.setTextureEntry(new SpriteCompressedTools("stone_axe", i, max_n));
-			map.setTextureEntry(new SpriteCompressedTools("stone_sword", i, max_n));
-			map.setTextureEntry(new SpriteCompressedTools("stone_paxel", new ResourceLocation(References.MOD_ID, "textures/items/stone_paxel.png"), i, max_n));
-		}
+			for (int i = 0; i < EnumCompressed.getLength(); i ++) {
+				stitchBar.step("Compressed tier " + (i + 1));
 
-		stitchBar.step("Stone Paxel");
-		// any JSON model files don't load this texture, so this texture isn't registered in the atlas
-		map.registerSprite(new ResourceLocation(References.MOD_ID, "items/stone_paxel"));
+				map.setTextureEntry(new SpriteCompressed("cobblestone", i, max_n));
+				map.setTextureEntry(new SpriteCompressedTools("stone_shovel", i, max_n));
+				map.setTextureEntry(new SpriteCompressedTools("stone_pickaxe", i, max_n));
+				map.setTextureEntry(new SpriteCompressedTools("stone_axe", i, max_n));
+				map.setTextureEntry(new SpriteCompressedTools("stone_sword", i, max_n));
+				map.setTextureEntry(new SpriteCompressedTools("stone_paxel", new ResourceLocation(References.MOD_ID, "textures/items/stone_paxel.png"), i, max_n));
+			}
 
-		ProgressManager.pop(stitchBar);
-		stopwatch.stop();
-		CompressedCobblestoneTools.LOGGER.info("Finish stitching textures : took " + stopwatch);
+			stitchBar.step("Stone Paxel");
+			// any JSON model files don't load this texture, so this texture isn't registered in the atlas
+			map.registerSprite(new ResourceLocation(References.MOD_ID, "items/stone_paxel"));
+
+			ProgressManager.pop(stitchBar);
+
+		});
 	}
 }
